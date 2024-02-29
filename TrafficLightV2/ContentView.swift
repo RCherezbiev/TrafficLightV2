25503215425063af69e28753f65ac19c6a8509c3
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-03-01 02:43:28 +0300

    End work with app

--- TrafficLightV2/ContentView.swift
+++ TrafficLightV2/ContentView.swift
@@ -7,15 +7,73 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, orange, green
+}
+
 struct ContentView: View {
+    @State private var currentLight = CurrentLight.red
+    @State private var redLight = 0.3
+    @State private var orangeLight = 0.3
+    @State private var greenLight = 0.3
+    
+    private var lightOn = 1.0
+    private let lightOff = 0.3
+
+    @State private var buttonText: String = "START"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 25) {
+            Circle()
+                .foregroundColor(.red)
+                .opacity(redLight)
+                .frame(width: 150, height: 150)
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            
+            Circle()
+                .foregroundColor(.orange)
+                .opacity(orangeLight)
+                .frame(width: 150, height: 150)
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            
+            Circle()
+                .foregroundColor(.green)
+                .opacity(greenLight)
+                .frame(width: 150, height: 150)
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            
+            Button(action: buttonAction) {
+                Text(buttonText)
+                    .frame(width: 150, height: 50)
+                    .background(Capsule().fill(.blue))
+                    .overlay(Capsule().stroke(Color.white, lineWidth: 4))
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+            }
+            .padding(.top, 100)
         }
-        .padding()
+    }
+    
+    private func buttonAction() {
+        if buttonText == "START" {
+            buttonText = "NEXT"
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLight = lightOff
+            redLight = lightOn
+            currentLight = .orange
+        case .orange:
+            redLight = lightOff
+            orangeLight = lightOn
+            currentLight = .green
+        case .green:
+            orangeLight = lightOff
+            greenLight = lightOn
+            currentLight = .red
+        }
+        
     }
 }
 
