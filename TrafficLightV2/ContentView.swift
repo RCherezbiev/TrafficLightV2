98fc5e66c9a5a55db64ce6cb0e02ac704ebe23ef
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-02-29 23:43:45 +0300

    Initial Commit

---
+++ TrafficLightV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrafficLightV2
+//
+//  Created by Rustam Cherezbiev on 29.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrafficLightV2/TrafficLightV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightV2App.swift
+//  TrafficLightV2
+//
+//  Created by Rustam Cherezbiev on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
